bb4948d86b8d766acf294a0e7a344303d87c4520
abhi17-96 <abhi.jadhav0396@gmail.com>
2024-11-22 19:24:00 +0530

    error handler fixed

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,13 +1,14 @@
 import Admin from "../models/adminModel.js"
+import {ApiError} from '../utils/ApiError.js'
+import { AsyncHandler } from "../utils/AsyncHandler.js";
 
-export const adminLogin = (req, res, next) => {
+export const adminLogin = AsyncHandler((req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    console.log("provide email and password")
-    next(Error("provide email and password"))
+    return next(new ApiError(400, "provide email and password"))
   }
 
   const admin = Admin.findOne({ email }).select('+password');
 
-} 
+})
